506056865d894db2eefbfea5d3cd0cac931c8163
Christian Pio Russo <christianpio.russo.stud@ispascalcomandini.it>
2025-10-27 08:57:20 +0100

    aggiunta dto

---
+++ Application/Dto/DtoAdopter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model.ValueObject;
+namespace Application.Dto
+{
+    public record class DtoAdopter
+    {
+        string Name;
+        string Username;
+        Address Address;
+        PhoneNumber PhoneNumber;
+        FiscalCode FiscalCode;
+
+    }
+}
---
+++ Application/Dto/DtoCat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model.Entities;
+namespace Application.Dto
+{
+    public record class DtoCat
+    {
+        string Name;
+        string Race;
+        Sex Sex;
+        string Description;
+        DateOnly? birth;
+        DateOnly arrived;
+        DateOnly? left;
+        string? catImage;
+        List<Adoption> cats;
+
+    }
+}
