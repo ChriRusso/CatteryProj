9b1bbf99dbf111b8f7065b5ae97d0212bd2627de
Christian Pio Russo <christianpio.russo.stud@ispascalcomandini.it>
2025-10-28 11:50:47 +0100

    inizio mappers

---
+++ Application/Mappers/MapperAdopter.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dto;
+using Domain.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class MapperAdopter
+    {
+        public static Adopter ToEntity(DtoAdoption dto) { 
+            return new DtoAdoption(
+                Cat:dto.Name,
+                )
+
+        }
+    }
+}
