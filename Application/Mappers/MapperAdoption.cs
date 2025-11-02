54e0e0cd1655e676827803c7975de2683e30a90f
ChriRusso <christianpio.russo.stud@ispascalcomandini.it>
2025-11-02 16:30:08 +0100

    aggiunta inziale dei mapper anche se non funzionante

--- Application/Mappers/MapperAdoption.cs
+++ Application/Mappers/MapperAdoption.cs
@@ -5,15 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using 
 namespace Application.Mappers
 {
-    public static class MapperAdopter
+    public static class MapperAdoption
     {
-        public static Adopter ToEntity(DtoAdoption dto) { 
-            return new DtoAdoption(
-                Cat:dto.Name,
-                )
+        public static Adopter ToEntity(DtoAdoption dto) {
+            return new DtoAdoption
+            {
+                Cat = DtoAdoption.Cat
+            }
+                
 
         }
     }
