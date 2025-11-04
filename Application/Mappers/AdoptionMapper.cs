402b6399dfed2d946f7fcafa83c60537c48779eb
ChriRusso <christianpio.russo.stud@ispascalcomandini.it>
2025-11-04 18:18:17 +0100

    test anche se non funzionano

---
+++ Application/Mappers/AdoptionMapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Application.Dto;
+using Domain.Model.Entities;
+
+namespace Application.Mappers
+{
+    public static class AdoptionMapper
+    {
+        public static Adoption ToEntity(this DtoAdoption dto)
+        {
+            if (dto == null) throw new ArgumentNullException(nameof(dto));
+
+           
+
+            return new Adoption(dto.Cat.ToEntity(), dto.Adopter, dto.date);
+        }
+
+        public static DtoAdoption ToDto(this Adoption adoption)
+        {
+            if (adoption == null) throw new ArgumentNullException(nameof(adoption));
+
+           
+
+            return new DtoAdoption(adoption.AdoptionCat.ToDto(), adoption.AdoptionAdopter, adoption.AdoptionDate);
+        }
+    }
+}
---
+++ DomainTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using src.Application.Mappers;
+using src.Application.Dto;
+namespace DomainTest
+{
+    public class Class1
+    {
+        [TestMethod]
+        public void AdoptionMapper_ToEntity_ThrowsOnNull()
+        {
+            DtoAdoption? nullDto = null;
+            Assert.ThrowsException<ArgumentNullException>(() => nullDto!.ToEntity());
+        }
+
+        [TestMethod]
+        public void AdoptionMapper_ToDto_ThrowsOnNull()
+        {
+            Adoption? nullEntity = null;
+            Assert.ThrowsException<ArgumentNullException>(() => nullEntity!.ToDto());
+        }
+    }
+}
